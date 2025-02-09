7e681242de34987fa7e9a12f637984cd12a2791c
FuraMex <uriel.22.a@gmail.com>
2025-02-08 21:38:01 -0700

    Wrote the GE01 assignment, Github...DON'T FUCK UP THE FILE THIS TIME

--- EclipseWorkSpace/CS1050Assignments/MartinezUrielGE01Calculations.java
+++ EclipseWorkSpace/CS1050Assignments/MartinezUrielGE01Calculations.java
@@ -4,7 +4,7 @@
  * Section: T / R
  * Feb 9 @ 11:59pm
  * Assignment 1
- * 
+ * This program will print a bit about myself and then calculate a grade
  */
 public class MartinezUrielGE01Calculations {
 
@@ -17,11 +17,26 @@ public class MartinezUrielGE01Calculations {
 		System.out.println("I don't live around MSU, as a result, I often have to commute by bus for about 30min the days I have classes.");
 		System.out.println("Commuting isn't all that bad however, it's actually quite nice! You don't have to worry about dealing with traffic and you can just sit on the bus and work on other things");
 	
-		System.out.println("\nSome personal things about myself is first of all I love to run.")
+		System.out.println("\nSome personal things about myself is first of all I love to run.");
 		System.out.println("Previously I had so many injuries that I couldn't keep up running for very long, but now I've found ways to keep myself injury-free.");
 		System.out.println("Another one of my hobbies is that I love to read. Currently I'm reading \"Born to Run\" by Christopher McDougall");
 		System.out.println("When I'm not outside running or reading a book, I also like to spend my time playing games. My current favorite is \"Laika: Aged Through Blood\"");
 		System.out.println("So I'm simply a geek studing Computer Science");
+		
+		final double CLASS_PARTICIPATION = .1;
+		final double GUIDED_EXPLORATIONS = .2;
+		final double QUIZZES = .25;
+		final double PROJECT_ITERATIONS = .25;
+		final double FINAL_EXAM = .2;
+		
+		double classParticiGrade = 89;
+		double geGrade = 83;
+		double quizGrade = 29;
+		double projectGrade = 59;
+		double finalExamGrade = 30;
+		
+		double finalGrade = (CLASS_PARTICIPATION * classParticiGrade) + (GUIDED_EXPLORATIONS * geGrade) + (QUIZZES * quizGrade) + (PROJECT_ITERATIONS * projectGrade) + (FINAL_EXAM * finalExamGrade);
+		System.out.printf("%s, your final grade is %.2f", "Uriel", finalGrade);
 	}
 
 }
