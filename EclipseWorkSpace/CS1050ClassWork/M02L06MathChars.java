94d178a13a8966f6136009919dc352155fa899cc
FuraMex <uriel.22.a@gmail.com>
2025-02-12 16:17:15 -0700

    demonstration update

---
+++ EclipseWorkSpace/CS1050ClassWork/M02L06MathChars.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class M02L06MathChars {
+
+	public static void main(String[] args) {
+		
+		String myString = "Hello World";
+		int myStringLength = myString.length();
+		System.out.println(myStringLength);
+	
+		char partOfString = myString.charAt(7);
+		System.out.print(partOfString);
+	
+	}
+
+}
