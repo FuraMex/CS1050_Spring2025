bad85b0a310c0fa240f59eabe4c77513b3c4c703
FuraMex <uriel.22.a@gmail.com>
2025-02-21 16:36:26 -0700

    Added some comments to programs and added a documentation outline

--- EclipseWorkSpace/CS1050Assignments/MartinezUrielGE01Calculations.java
+++ EclipseWorkSpace/CS1050Assignments/MartinezUrielGE01Calculations.java
@@ -6,11 +6,12 @@
  * Assignment 1
  * This program will print a bit about myself and then calculate a grade
  */
-public class MartinezUrielGE01Calculations {
+public class MartinezUrielGE01Calculations 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		A little bit about myself
+	public static void main(String[] args) 
+	{
+		// A little bit about myself
 		System.out.println("I'm Uriel Martinez and I'm studying to earn a degree in Computer Science.");
 		System.out.println("This isn't my first year in college as I've been attending front range so I could complete my general courses.");
 		System.out.println("However, this is my first year at MSU where I'll finally be doing my actual CS classes.");
@@ -23,20 +24,25 @@ public class MartinezUrielGE01Calculations {
 		System.out.println("When I'm not outside running or reading a book, I also like to spend my time playing games. My current favorite is \"Laika: Aged Through Blood\"");
 		System.out.println("So I'm simply a geek studing Computer Science");
 		
+		// declaring and initializing the variables for percentage weights
 		final double CLASS_PARTICIPATION = .1;
 		final double GUIDED_EXPLORATIONS = .2;
 		final double QUIZZES = .25;
 		final double PROJECT_ITERATIONS = .25;
 		final double FINAL_EXAM = .2;
 		
+		// declared the student grade variables and added test values
 		double classParticiGrade = 89;
 		double geGrade = 83;
 		double quizGrade = 29;
 		double projectGrade = 59;
 		double finalExamGrade = 30;
+			
+		double breakingCode = 1 / 0;
 		
+		// declaring then initializing it with the final calculated grade
 		double finalGrade = (CLASS_PARTICIPATION * classParticiGrade) + (GUIDED_EXPLORATIONS * geGrade) + (QUIZZES * quizGrade) + (PROJECT_ITERATIONS * projectGrade) + (FINAL_EXAM * finalExamGrade);
 		System.out.printf("\n%s, your final grade is %.2f", "Uriel", finalGrade);
-	}
+	} // end of main method
 
-}
+} // end of public class
