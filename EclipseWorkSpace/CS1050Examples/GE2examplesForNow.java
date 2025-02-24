6e07089d7c2896be4d05898238ed8d1abd1d93d4
FuraMex <uriel.22.a@gmail.com>
2025-02-24 14:40:41 -0700

    added an example program for use

--- EclipseWorkSpace/CS1050Examples/GE2examplesForNow.java
+++ EclipseWorkSpace/CS1050Examples/GE2examplesForNow.java
@@ -6,16 +6,6 @@ public class GE2examplesForNow
 	{
 		
 		
-		int testVar = 3;
-		
-		if (testVar % 2 == 0) 
-		{
-			System.out.println("The number is even");
-		}
-		else
-		{
-			System.out.println("The number is odd");
-		} // end of if statement
 
 	} // main method
 
---
+++ EclipseWorkSpace/CS1050Examples/exampleCode.java
@@ -0,0 +1,19 @@
+
+public class exampleCode 
+{
+
+	public static void main(String[] args) 
+	{
+		int caseVal = 1;
+		
+		switch (caseVal)
+		{
+			case 1: 
+				System.out.print("The number is 1");
+				break;
+			case 2:
+				System.out.print("The number is 2");
+				break;
+		}
+	}
+}
